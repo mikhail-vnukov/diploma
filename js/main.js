636879338d3a4d16ba34f44e1d77cf26814497d1
Mikhail Vnukov <mikhail.vnukov@gmail.com>
2012-12-06 01:36:46 +0400

    Fixed minor issues

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ function resize(load) {
 	var siteHeight = $(".site").height();
 	var footerHeight = $(".footer").height();
 	var headerHeight = $(".header").height();
-	var autoheight = $(".container").data("height");
+	var autoheight = $(".content").data("height");
 	if(!autoheight) {
-		autoheight = $(".container").height();
-		$(".container").data("height", autoheight);
+		autoheight = $(".content").height();
+		$(".content").data("height", autoheight);
 	}
 
 	var newHeight = siteHeight - footerHeight - headerHeight;
